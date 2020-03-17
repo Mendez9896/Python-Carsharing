a6fec242e0c0bdb6aaf537f0c416063242003d75
Mateo Altamirano Vega <mateo.altamirano.1998@gmail.com>
2020-03-17 08:32:19 -0400

    all basic views and styles

--- carsharing/mysite/carsharing/urls.py
+++ carsharing/mysite/carsharing/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sign-in', views.signIn, name='sign-in'),
+    path('log-in', views.logIn, name='log-in'),
     path('perfil', views.perfil, name='perfil'),
     path('single-product', views.singleProduct, name='single-product'),
+    path('add-car', views.addCar, name='add-car'),
+    path('rent-a-car', views.rentACar, name='rent-a-car'),
+
 ]
--- carsharing/mysite/carsharing/views.py
+++ carsharing/mysite/carsharing/views.py
@@ -3,8 +3,20 @@ from django.shortcuts import render
 def index(request):
     return render(request,'carsharing/index.html',{})
 
+def signIn(request):
+    return render(request,'carsharing/signIn.html',{})
+
+def logIn(request):
+    return render(request,'carsharing/logIn.html',{})
+
 def perfil(request):
     return render(request,'carsharing/profile.html',{})
 
 def singleProduct(request):
     return render(request,'carsharing/single-product.html',{})
+
+def addCar(request):
+    return render(request,'carsharing/addCar.html',{})
+
+def rentACar(request):
+    return render(request,'carsharing/rentAcar.html',{})
