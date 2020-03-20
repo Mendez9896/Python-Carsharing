391f1bdf166fc6b6988831847c6ca1776a07eee7
Mateo Altamirano Vega <mateo.altamirano.1998@gmail.com>
2020-03-19 20:35:27 -0400

    models

--- carsharing/mysite/carsharing/models.py
+++ carsharing/mysite/carsharing/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=50)
+
+class Vehiculo(models.Model):
+    idPropietario = models.ForeignKey(Usuario,on_delete=models.CASCADE)
+    descripcion = models.TextField()
+    marca = models.CharField(max_length=50)
+    modelo = models.CharField(max_length=50)
+    año = models.IntegerField()
+    disponible = models.BooleanField()
+    ciudad = models.ForeignKey(Ciudad,on_delete=models.CASCADE)
+
+class Alquiler(models.Model):
+    idVehiculo = models.ForeignKey(Vehiculo,on_delete=models.CASCADE)
+    precio = models.IntegerField()
+    inicio = models.DateField()
+    final = models.DateField()
