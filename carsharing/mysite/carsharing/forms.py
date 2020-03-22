f2291d618b16a956121a4f0910263663447c7f33
Mateo Altamirano Vega <mateo.altamirano.1998@gmail.com>
2020-03-22 18:53:40 -0400

    add vehicle fix ciudad

--- carsharing/mysite/carsharing/forms.py
+++ carsharing/mysite/carsharing/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 import datetime
 max_año = int(datetime.datetime.now().year) + 1
+CIUDADES = (('La Paz', 'La Paz'),('Oruro', 'Oruro'),('Potosi','Potosi'),('Cochabamba','Cochabamba'),('Tarija','Tarija'),('Chuquisaca','Chuquisaca'),('Santa Cruz','Santa Cruz'),('Beni','Beni'),('Pando','Pando'),)
 class AddVehicle(forms.Form):
     foto = forms.ImageField(allow_empty_file=True)
     descripcion = forms.CharField()
@@ -8,3 +9,4 @@ class AddVehicle(forms.Form):
     modelo = forms.CharField()
     año = forms.IntegerField(min_value=1980, max_value=max_año)
     precio = forms.IntegerField()
+    ciudad = forms.ChoiceField(choices=CIUDADES)
